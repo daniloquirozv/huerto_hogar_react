880b282f6ddbc120ec201295bca7d3bed764003b
dan <dan.quirozv@duocuc.cl>
2025-10-04 19:36:28 -0300

    Modificando primeros assets y components

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import productoStyles from '../assets/styles/producto-styles.css'
+
+
+
+
+function HeaderComponent() {
+    return (
+        <Navbar expand="lg" className={` ${productoStyles.navbar} `}>
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">                        
+                        <Nav.Link as={Link} to={'/producto'}>Contacto</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>        
+    );
+}
+export default HeaderComponent;
---
+++ src/pages/ProductoPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeaderComponent from '../components/HeaderComponent'
+
+function ProductoPage() {
+  return (
+    <>
+        <HeaderComponent></HeaderComponent>
+        <div>Producto Page</div>
+    </>
+  )
+}
+
+export default ProductoPage
