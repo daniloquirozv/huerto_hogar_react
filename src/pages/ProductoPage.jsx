25dbcf9d21d4082c0952a4bf03217d315ea3d192
dan <dan.quirozv@duocuc.cl>
2025-10-06 15:33:29 -0300

    Cards productos listas

--- src/pages/ProductoPage.jsx
+++ src/pages/ProductoPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderComponent from '../components/HeaderComponent'
 import FooterComponent from '../components/FooterComponent'
+import CardsComponent from '../components/CardsComponent'
 
 function ProductoPage() {
   return (
     <>
         <HeaderComponent></HeaderComponent>
         <div>Producto Page</div>
+        <CardsComponent></CardsComponent>
         <FooterComponent></FooterComponent>
     </>
   )
