599f44a762848b5363c92b808bcb44d745d9aa2b
dan <dan.quirozv@duocuc.cl>
2025-10-07 13:20:43 -0300

    Navbar actualizado

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -24,15 +24,16 @@ function HeaderComponent() {
     return (
         <Navbar expand="lg" className={` ${productoStyles.navbar} `}>
             <Container>
-                <Navbar.Brand href="#home"><img src={logotipo} alt="Logotipo" title='Huerto Hogar' width={100} /></Navbar.Brand>
+                <Navbar.Brand href=""><img src={logotipo} alt="Logotipo" title='Huerto Hogar' width={100} /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">                        
-                        <Nav.Link href='#Contacto' onClick={handleContactoClick}>Contacto</Nav.Link>
-                    </Nav>
-                    <Nav>
+                    <Nav className="me-auto ">                        
                         <Nav.Link as={Link} to={'/producto'}>Inicio</Nav.Link>
-                    </Nav>
+                        <Nav.Link as={Link} to={'/producto'}>Productos</Nav.Link>
+                        <Nav.Link as={Link} to={'/producto'}>Blog</Nav.Link>
+                        <Nav.Link href='#Contacto' onClick={handleContactoClick}>Contacto</Nav.Link>
+                        <Nav.Link as={Link} to={'/producto'}>Inicio Sesion</Nav.Link>
+                    </Nav>             
                 </Navbar.Collapse>
             </Container>
         </Navbar>        
